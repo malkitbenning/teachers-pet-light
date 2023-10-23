8af02a3bed072de91f221035075af5387de12c60
malkitbenning <malkitbenning@gmail.com>
2023-10-23 19:42:12 +0100

    put back fetches

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -114,4 +114,3 @@ function LandingPage() {
 }
 
 export default LandingPage;
-
--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -4,7 +4,7 @@ import LoginButton from "./LoginButton";
 import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
-  const apiURL = process.env.REACT_APP_DEV_URL || "http://13.40.157.9";
+  const apiURL = process.env.REACT_APP_DEV_URL || "http://13.40.157.9/";
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [usernameError, setUsernameError] = useState("");
@@ -50,9 +50,7 @@ function LoginForm() {
             if (data) {
               setValidationError(data.message);
             } else {
-              setValidationError(
-                "An error occurred while processing your request."
-              );
+              setValidationError("An error occurred while processing your request.");
             }
           });
         }
